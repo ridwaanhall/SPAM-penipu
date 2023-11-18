bcb7550f0c742ba67ae5c730f7bf8ffb4308e184
ridwaanhall <erbyl14@gmail.com>
2023-11-19 00:03:23 +0700

    part 7 - episode 1

--- 01_EPISODE/otp_phone.py
+++ 01_EPISODE/otp_phone.py
@@ -1,7 +1,8 @@
 import requests, random
 
 url_phone = 'http://kerjabumn-com.preview-domain.com/req/phone.php'
-url_otp = 'http://kerjabumn-com.preview-domain.com/req/code.php'
+url_otp_apk = 'http://kerjabumn-com.preview-domain.com/req/code.php' # 5 pin
+url_otp_sms = 'http://kerjabumn-com.preview-domain.com/req/code2.php' # 6 pin
 
 headers = {
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
@@ -29,17 +30,25 @@ for loop in range(1, loop_value+1):
     random_pin3 = str(random.randint(0, 9))
     random_pin4 = str(random.randint(0, 9))
     random_pin5 = str(random.randint(0, 9))
+    random_pin6 = str(random.randint(0, 9))
 
     data_otp = {
         'pin1': random_pin1,
         'pin2': random_pin2,
         'pin3': random_pin3,
         'pin4': random_pin4,
-        'pin5': random_pin5
+        'pin5': random_pin5,
+        'pin6': random_pin6
     }
     
-    response = requests.post(url_otp, data=data_otp, headers=headers)
+    response = requests.post(url_otp_sms, data=data_otp, headers=headers)
     print('pin:', random_pin1 + random_pin2 + random_pin3 + random_pin4 + random_pin5)
-    print(response.status_code)
+    # print('status code:',response.status_code)
+    # print('cookies:',response.cookies)
+    # print('', response.headers)
+    # print(response.elapsed)
+    # print(response.apparent_encoding)
+    # print(response.close)
+    # print(response.content)
     print('')
     
