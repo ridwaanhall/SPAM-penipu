eb018658ceb951e6f7d5943cda4e0de72436031c
ridwaanhall <erbyl14@gmail.com>
2023-11-18 17:39:42 +0700

    part 4 - episode 1

---
+++ 01_EPISODE/01_LINK TIPU/04_part.py
@@ -0,0 +1,35 @@
+import requests
+import random
+
+# from https://grupswhatsp.safqd.com/p/?s=2d7d0731 redirect to https://djngk.terbaiik.com/
+url = 'https://djngk.terbaiik.com/data.php'
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+}
+loop_value = int(input("input number of loop : "))
+
+for loop in range(1, loop_value+1):
+    '''
+    email: dsadasdas
+    sandi: dsadasdasdad
+    login: Facebook
+    '''
+    # random of k
+    k_random_email = random.randint(10, 20)
+    k_random_sandi = random.randint(10, 100)
+    
+    # create random email
+    random_email = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz0123456789_.-', k=k_random_email)) + '@gmail.com'
+    random_sandi = ''.join(random.choices("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-={}[]|:;'<>,.?/", k=k_random_sandi))
+    
+    data = {
+        'email': random_email,
+        'sandi': random_sandi*10000,
+        'login': 'Facebook'
+    }
+    
+    response = requests.post(url, data=data, headers=headers)
+    # make loop in format 0000000001
+    loop = str(loop).zfill(10)
+    print(f"{loop}. " + random_email)
+    print(response.text)
