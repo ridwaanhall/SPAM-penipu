6c538c31d3dcea867138f911688c232d1b1be0f6
ridwaanhall <erbyl14@gmail.com>
2023-11-18 13:43:05 +0700

    part 3 - episode 1

---
+++ 01_EPISODE/01_LINK TIPU/03_part.py
@@ -0,0 +1,32 @@
+import requests
+import random
+
+url = 'https://sskss.mujxk.com/data.php'
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+}
+loop_value = int(input("bar bar brp kali? : "))
+
+for loop in range(1, loop_value+1):
+    '''
+    email: dsadasdas
+    sandi: dsadasdasdad
+    login: Facebook
+    '''
+    # random of k
+    k_random_email = random.randint(10, 20)
+    k_random_sandi = random.randint(10, 10)
+    
+    # create random email
+    random_email = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz0123456789_.-', k=k_random_email)) + '@gmail.com'
+    random_sandi = ''.join(random.choices("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-={}[]|:;'<>,.?/", k=k_random_sandi))
+    
+    data = {
+        'email': random_email,
+        'sandi': random_sandi*10000,
+        'login': 'Facebook'
+    }
+    
+    response = requests.post(url, data=data, headers=headers)
+    print(f"{loop} | random email:" + random_email)
+    print(response.text)
